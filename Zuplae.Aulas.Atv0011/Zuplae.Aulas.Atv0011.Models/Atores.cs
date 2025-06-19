11684f09282cea27374a965eb2c32171e7220a0a
Daniela2319 <frankvt04@gmail.com>
2025-06-18 21:38:03 -0300

    Criada a estrutura do projeto

---
+++ Zuplae.Aulas.Atv0011/Zuplae.Aulas.Atv0011.Models/Atores.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zuplae.Aulas.Atv0011.Models
+{
+    internal class Atores
+    {
+        public string nome;
+        public int idade;
+        public string nacionalidade;
+        public Filme filme;
+
+        public Atores(string nome, int idade, string nacionalidade, Filme filme)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.nacionalidade = nacionalidade;
+            this.filme = filme;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {nome}, Idade: {idade}, Nacionalidade: {nacionalidade}, Filme: {filme}";
+        }
+    }
+}
